3c13884c0563de2d34421aedf3a6607872d6532b
Shresth-11 <jaisshresth143@gmail.com>
2026-05-28 09:49:59 +0530

    Build: Upgrade pip and specify python version in runtime.txt to pull binary wheels of pandas and prevent slow compilation from source

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,9 @@
 # Render build script for Django backend
 set -o errexit
 
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py seed
+
